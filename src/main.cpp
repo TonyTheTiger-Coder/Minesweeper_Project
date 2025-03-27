e02ac037850abfcf3b23df988aa891c87f842c5f
TonyTheTiger-Coder <willwadsworth69@gmail.com>
2025-03-27 19:54:31 -0400

    Added ESC key close and window.clear

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,30 @@
-//this is a comment
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf :: RenderWindow window;
+    window.create(sf :: VideoMode(), "Minesweeper", sf :: State :: Fullscreen);
+    window.setFramerateLimit(60);
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std :: optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf :: Event::Closed>())
+                window.close();
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+            }
+
+        }
+        window.clear(sf :: Color::Black);
+
+        window.display();
+    }
+}
